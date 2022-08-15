77d5d1b65c8a41a1b172acb718758a8cb25ade1c
William Sundgård Anker <will1331@edu.sde.dk>
2022-08-15 13:12:41 +0200

    Deleting Models

--- bestillingsformularForBiludlejningsfirmaH3/Models/BookingVehicles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace bestillingsformularForBiludlejningsfirmaH3.Models
-{
-    public class BookingVehicles
-    {
-        public int booking_vehicle_id { get; set; }
-        public string booking_vehicle { get; set; }
-        public double booking_vehicle_price { get; set; }
-    }
-}
